4f3a22bac4e6b97439ffd47ad3b46a93bebbafe0
inrage <inrag3x@gmail.com>
2023-12-13 11:40:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def simple_application():
+    # Perform some calculations
+    number_1 = 10
+    number_2 = 5
+    sum_result = number_1 + number_2
+    difference = number_1 - number_2
+    product = number_1 * number_2
+    
+    # Display the results (you can comment out to suppress output)
+    print("Sum:", sum_result)
+    print("Difference:", difference)
+    print("Product:", product)
+
+
+if __name__ == "__main__":
+    simple_application()
