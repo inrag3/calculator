f9d8866e3179c7be93b4e16140865a3b6315471a
inrage <inrag3x@gmail.com>
2023-12-13 12:58:20 +0300

    Add tests

---
+++ tests.py
@@ -0,0 +1,29 @@
+from calculator import multiply, division, difference, sum
+
+# Test multiply function
+def test_multiply():
+    assert multiply(3, 4) == 12, "Test Case 1 Failed"
+    assert multiply(0, 7) == 0, "Test Case 2 Failed"
+
+# Test division function
+def test_division():
+    assert division(10, 2) == 5, "Test Case 1 Failed"
+    assert division(8, 4) == 2, "Test Case 2 Failed"
+
+# Test difference function
+def test_difference():
+    assert difference(8, 3) == 5, "Test Case 1 Failed"
+    assert difference(0, 7) == -7, "Test Case 2 Failed"
+
+# Test sum function
+def test_sum():
+    assert sum(4, 7) == 11, "Test Case 1 Failed"
+    assert sum(0, 0) == 0, "Test Case 2 Failed"
+
+# Run the tests
+if __name__ == "__main__":
+    test_multiply()
+    test_division()
+    test_difference()
+    test_sum()
+    print("All tests passed!")
